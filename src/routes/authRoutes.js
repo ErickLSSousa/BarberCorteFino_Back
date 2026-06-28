da3ecff82695ef52416766db6d0c59490daa0aa5
ErickLSSousa <erickrewardd@gmail.com>
2026-06-28 17:09:33 -0300

    Add client authentication endpoints

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
-const { login } = require("../controllers/authController");
+const {
+  loginAdmin,
+  loginClient,
+  registerClient,
+} = require("../controllers/authController");
 const validate = require("../middlewares/validate");
-const { loginSchema } = require("../schemas/adminSchemas");
+const { clientRegisterSchema, loginSchema } = require("../schemas/authSchemas");
 
 const router = Router();
 
-router.post("/login", validate(loginSchema), login);
+router.post("/login", validate(loginSchema), loginAdmin);
+router.post("/admin/login", validate(loginSchema), loginAdmin);
+router.post("/client/register", validate(clientRegisterSchema), registerClient);
+router.post("/client/login", validate(loginSchema), loginClient);
 
 module.exports = router;
-
